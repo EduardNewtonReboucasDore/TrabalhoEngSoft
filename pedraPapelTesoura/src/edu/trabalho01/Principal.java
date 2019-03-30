e423fedb309006db688cabb32191eaf33bb5809f
Eduard <Eduard@DESKTOP-1SAIKSP>
2019-03-30 15:06:43 -0300

    Commit Test

--- pedraPapelTesoura/src/edu/trabalho01/Principal.java
+++ pedraPapelTesoura/src/edu/trabalho01/Principal.java
@@ -12,7 +12,7 @@ public class Principal {
 		
 		for(int i = 1; i < 4; i++) {
 			for(int j = 3; j > 0; j --) {
-				System.out.printf("%d %d: ",i,j);
+				//System.out.printf("%d %d: ",i,j);
 				j1.getGanhador(i, j);
 			}
 		}
